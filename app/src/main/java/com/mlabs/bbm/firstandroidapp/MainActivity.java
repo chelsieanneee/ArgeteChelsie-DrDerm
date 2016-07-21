a81f2c1452285ef0fbd3f5456706f2a77bf9d961
john <jvaldoria413@yahoo.com>
2016-07-21 19:47:59 +0800

    (modified splash screen, login and display)

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -1,13 +1,76 @@
 package com.mlabs.bbm.firstandroidapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import  android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    EditText Uname;
+    EditText Pwd;
+    Button btnLogs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void onButtonClick(View v)
+    {
+        if(v.getId() ==  R.id.btnLog)
+        {
+            EditText a = (EditText)findViewById(R.id.TFUname);
+            EditText p = (EditText)findViewById(R.id.TFPword);
+            Intent i = new Intent(MainActivity.this, Display.class);
+
+            String uname = a.getText().toString();
+            String pword = p.getText().toString();
+
+            boolean b = Pattern.matches("[a-zA-Z0-9]{4,16}", uname);
+            if(b == true)
+            {
+
+                i.putExtra("Username",uname);
+                startActivity(i);
+            }
+            else
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(" Username or Password Must Be :" +  "\n" +
+                        " ALPHANUMERIC" + "\n" +
+                        " 8 to 24 Characters");
+                builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+            }
+        }
+    }
+
+    public void clickExit(View x)
+    {
+        finish();
+    }
+
 }
+
+
--- app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
@@ -1,3 +1,5 @@
+
+
 package com.mlabs.bbm.firstandroidapp;
 
 import android.app.Activity;
@@ -7,27 +9,28 @@ import android.os.Bundle;
 /**
  * Created by benjarmanalili on 16/07/2016.
  */
+
 public class SplashScreen extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-    Thread timerThread = new Thread(){
-        public void run(){
-            try{
-                sleep(3000);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class );
+                    startActivity(intent);
+                }
             }
-            catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            finally{
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class );
-                startActivity(intent);
-            }
-        }
-    };
+        };
         timerThread.start();
     }
 
