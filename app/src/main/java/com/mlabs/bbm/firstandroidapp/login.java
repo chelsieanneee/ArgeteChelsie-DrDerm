90d8c12d3747772a6b52bd82d6598ee0370e0792
Chelsie Anne Argete <chelsie.anneargete@outlook.com>
2016-08-27 18:49:29 +0800

    Final

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/login.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/login.java
@@ -3,6 +3,8 @@ package com.mlabs.bbm.firstandroidapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
@@ -18,31 +20,57 @@ public class login extends AppCompatActivity {
     EditText Email;
     EditText Password;
     Button LogIn;
+    Button Show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        Email=(EditText) findViewById(R.id.tfEmail);
-        Password=(EditText) findViewById(R.id.tfPassword);
-        LogIn=(Button) findViewById(R.id.btLogIn);
+        Email = (EditText) findViewById(R.id.tfEmail);
+        Password = (EditText) findViewById(R.id.tfPassword);
+        LogIn = (Button) findViewById(R.id.btLogIn);
+        Show = (Button) findViewById(R.id.btShowpass);
         LogIn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Pattern.compile("^\\w+.*\\w*@[a-zA-Z_]+?\\.[0-9a-zA-Z]{2,}$").matcher(Email.getText()).matches() && Password.length()>=8){
-                        Intent intent= new Intent(login.this,MainActivity.class);
-                        startActivity(intent);
-                    }
-                else{
-                    Toast.makeText(getBaseContext(),"Email or Password is Incorrect",Toast.LENGTH_SHORT).show();
+                if (Pattern.compile("^\\w+.*\\w*@[a-zA-Z_]+?\\.[0-9a-zA-Z]{2,}$").matcher(Email.getText()).matches() && Password.length() >= 8) {
+                    Intent intent = new Intent(login.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getBaseContext(), "Email or Password is Incorrect", Toast.LENGTH_SHORT).show();
 
                 }
 
             }
         });
+        Show.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionevent) {
+
+                int event = motionevent.getAction();
+
+                // if (event == motionevent.ACTION_DOWN) {
+                //    Password.setTransformationMethod(null);
+                //}
+                //  if (event == motionevent.ACTION_UP) {
+                //    Password.setTransformationMethod(new PasswordTransformationMethod());
+                //}
+
+                switch (motionevent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Password.setTransformationMethod(null);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        Password.setTransformationMethod(new PasswordTransformationMethod());
+                        break;
+                }
+                return true;
+
+            }
+        });
 
     }
-    @Override
     protected  void onPause(){
         super.onPause();
         finish();
